de791ca231b9a1a69f75c6e86eac7afba50abc83
Niceygy <niceygy@niceygylive.xyz>
2024-01-28 18:55:11 +0000

    finished. that was easy xD

--- src/index.js
+++ src/index.js
@@ -11,14 +11,71 @@
 //FORMAT:
 //stats.thealiendoctor.com?page=/ufo-studios.html
 
+async function scheduled(env, sendMsg) {
+	console.log('Cron func is running...');
+	const KV = env.KV;
+	const webhookURL = env.url;
+	var pages = [];
+
+	const list = await KV.list();
+	console.log('Key list aquired');
+	var total = 0;
+	for (const { name, expiration } of list.keys) {
+		console.log('Getting value for ' + name);
+		const value = await KV.get(name);
+		if ((name == 'total') | (name == undefined) | (name == 'undefined')) {
+			console.log('Invalid Entry, skipping');
+		} else {
+			pages.push({ name: name, count: parseInt(value) });
+			total += parseInt(value);
+		}
+	}
+	pages.sort((a, b) => b.count - a.count); // Sort repos by count in descending order
+	let currentDate = new Date();
+
+	var message =
+		'# Page Views as of ' +
+		currentDate.toUTCString() +
+		'\n\n' +
+		pages.map((pages) => `Page: ${pages.name}, Count: ${pages.count}\n`).join('');
+	var message = message + '\n\nTotal views: ' + total;
+	console.log('Message generated. Sending to Discord...');
+	console.log(message);
+
+	if (sendMsg) {
+		await fetch(webhookURL, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				content: message,
+			}),
+		})
+			.then((response) => console.log(response))
+			.catch((error) => console.error(error));
+		console.log('Message sent to Discord');
+		return 'Complete! Message sent.';
+	} else {
+		console.log('Returning mesage');
+		return message;
+	}
+}
+
 export default {
 	async fetch(request, env, ctx) {
 		let url = request.url;
 		let KV = env.KV;
-		console.log("Handling request for: " + url);
-		let page = url.split("?page=")[1];
+		console.log('Handling request for: ' + url);
+		if (url == 'https://stats.thealiendoctor.com') {
+			return new Response('No page specified', { status: 400 });
+		}
+		if (url == 'https://stats.thealiendoctor.com/cron' || url == "127.0.0.1:8787/cron") {
+			return new Response(await scheduled(env, false), { status: 200 });
+		}
+		let page = url.split('?page=')[1];
 		if (page == undefined) {
-			return new Response("No page specified", { status: 400 });
+			return new Response('No page specified', { status: 400 });
 		}
 		let count = await KV.get(page);
 		if (count == null) {
@@ -26,55 +83,9 @@ export default {
 		}
 		count++;
 		await KV.put(page, count);
-		return new Response("OK", { status: 200 });
+		return new Response('OK', { status: 200 });
 	},
-async cron(env, ctx){
-async function cron(env, sendMsg) {
-  console.log("Cron func is running...");
-  const KV = env.KV;
-  const webhookURL = env.url;
-  var repos = [];
-
-  const list = await KV.list();
-  console.log("Key list aquired");
-  var total = 0;
-  for (const { name, expiration } of list.keys) {
-    console.log("Getting value for " + name);
-    const value = await KV.get(name);
-    if (name == "total" | name == undefined | name == "undefined") {
-      console.log("Invalid Repo, skipping");
-    } else {
-      repos.push({ name: name, count: parseInt(value) });
-      total += parseInt(value);
-    }
-  }
-  repos.sort((a, b) => b.count - a.count); // Sort repos by count in descending order
-  let currentDate = new Date();
-
-  var message = "# Downloads as of " + currentDate.toUTCString() + "\n\n" + repos.map(repo => `Repo: ${repo.name}, Count: ${repo.count}\n`).join("");
-  var message = message + "\n\nTotal downloads: " + total;
-  console.log("Message generated. Sending to Discord...");
-  console.log(message);
-
-  if (sendMsg == false) {
-  await fetch(webhookURL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      content: message,
-    }),
-  })
-    .then((response) => console.log(response))
-    .catch((error) => console.error(error));
-  console.log("Message sent to Discord");
-  return "Complete! Message sent.";
-} else {
-  console.log("Returning mesage");
-  return message;
-}
-}
-cron(env, True)
-}
+	async cron(env, ctx) {
+		scheduled(env, True);
+	},
 };
